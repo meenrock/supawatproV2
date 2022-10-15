2bfa82e1dba79bfbc68e8102aecab0ce6916954f
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2022-10-15 19:01:01 +0700

    add component for bg

---
+++ components/background/wavyBg.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/wavy.component.css'
+
+const wavyBg = () => {
+    <div className='ocean'>
+        <div className='wave'></div>
+        <div className='wave'></div>
+    </div>
+}
+
+export default function wavyBg() {
+    return (
+        <>
+        </>
+    )
+}
